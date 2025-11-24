ffdc3955de95f2a1df53dac632891a96b4e5d87b
Heizel1228 <ansonchan19991228@gmail.com>
2025-11-24 15:59:14 +0800

    Update - Shader/Object Select Outline
    Never gone give you up

--- Assets/Scripts/Highlightable.cs
+++ Assets/Scripts/Highlightable.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Highlightable : MonoBehaviour
 {
-    [SerializeField] private Material highlight;
+    // Drag the Material Asset you want to target here
+    [SerializeField] private Material targetMaterial;
     [SerializeField] private MeshRenderer meshRenderer;
 
-    private Material regular;
+    [SerializeField] private bool playerObjbool;
+
+    private MaterialPropertyBlock _propBlock;
 
     private void OnValidate()
     {
@@ -15,13 +18,45 @@ public class Highlightable : MonoBehaviour
             meshRenderer = GetComponent<MeshRenderer>();
     }
 
-    private void Start()
+    private void Awake()
     {
-        regular = meshRenderer.material;
+        _propBlock = new MaterialPropertyBlock();
     }
 
     public void Highlight(bool doHighlight)
     {
-        meshRenderer.material = doHighlight ? highlight : regular;
+        if (meshRenderer == null || targetMaterial == null) return;
+
+        Material[] currentMaterials = meshRenderer.sharedMaterials;
+
+        for (int i = 0; i < currentMaterials.Length; i++)
+        {
+            if (currentMaterials[i] == targetMaterial)
+            {
+                ApplyHighlight(i, doHighlight);
+            }
+        }
+    }
+
+    private void ApplyHighlight(int index, bool active)
+    {
+        meshRenderer.GetPropertyBlock(_propBlock, index);
+
+        if (active)
+        {
+            if(playerObjbool)
+                _propBlock.SetFloat("_outline_Scale", 1.05f);
+            else
+                _propBlock.SetFloat("_outline_Scale", 1.03f);
+        }
+        else
+        {
+            if (playerObjbool)
+                _propBlock.SetFloat("_outline_Scale", 0f);
+            else
+                _propBlock.SetFloat("_outline_Scale", 1f);
+        }
+
+        meshRenderer.SetPropertyBlock(_propBlock, index);
     }
 }
